9b9f0a4bd3aa3349f9d896b4829984a499f98ecb
Benjamin Hansen <bhansen@zork.local>
2015-10-22 17:38:49 -0500

    small touches, might need a color check on the grid

--- Assets/UI/UI_Scripts/TextureAnimator.cs
+++ Assets/UI/UI_Scripts/TextureAnimator.cs
@@ -10,6 +10,12 @@ public class TextureAnimator : MonoBehaviour {
 
 	private float index;
 
+	void Start () {
+		for (int i = 0; i < frames.Length; i++) {
+			frames[i] = Resources.Load ("MainMenuAnimation/mm00" + (i + 111)) as Texture2D;
+		}
+	}
+
 	void Update () {
 		index = Time.time * framesPerSecond;
 		index = index % frames.Length;
