e988420bf6b4ae8d4fa09155f0b4753b50e68f3a
XanGrab <xander@xangrab.com>
2024-12-19 11:45:53 -0600

    Vault button

---
+++ Assets/JSPlugin/VaultDropdownToggle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Note: This class is designed to be used with the `vault-floating-dropdown` jslib pluggin. 
+/// This script will remove the dropdown elment from the DOM after the target scene has been unloaded.
+/// </summary>
+public class VaultDropdownToggle : MonoBehaviour {
+    [SerializeField]
+    private MainMenuPanel menu;
+
+    [DllImport("__Internal")]
+    private static extern void DisableVaultButton();
+
+    private void OnEnable() {
+        menu.levelsButton.onClick.AddListener(OnMenuExit);
+		menu.creditsButton.onClick.AddListener(OnMenuExit);
+		menu.freePlayButton.onClick.AddListener(OnMenuExit);
+    }
+
+    private void OnMenuExit() {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        DisableVaultButton();
+#endif
+    }
+}
